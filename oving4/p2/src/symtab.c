357baf0c204d78b15ae4b8f7cf423d7f9fc66129
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-03-14 04:44:13 +0100

    OVING4-P2: Formatted the P2-source in a sane fashion

--- oving4/p2/src/symtab.c
+++ oving4/p2/src/symtab.c
@@ -10,57 +10,49 @@ static int32_t strings_size = 16, strings_index = -1;
 
 
 void
-symtab_init ( void )
-{
+symtab_init(void) {
 }
 
 
 void
-symtab_finalize ( void )
-{
+symtab_finalize(void) {
 }
 
 
 int32_t
-strings_add ( char *str )
-{
-    return 0;
+strings_add(char *str) {
+	return 0;
 }
 
 
 void
-strings_output ( FILE *stream )
-{
+strings_output(FILE *stream) {
 }
 
 
 void
-scope_add ( void )
-{
+scope_add(void) {
 }
 
 
 void
-scope_remove ( void )
-{
+scope_remove(void) {
 }
 
 
 void
-symbol_insert ( char *key, symbol_t *value )
-{
+symbol_insert(char *key, symbol_t *value) {
 #ifdef DUMP_SYMTAB
-fprintf ( stderr, "Inserting (%s,%d)\n", key, value->stack_offset );
+	fprintf(stderr, "Inserting (%s,%d)\n", key, value->stack_offset);
 #endif
 }
 
 
 void
-symbol_get ( symbol_t **value, char *key )
-{
-    symbol_t *result = NULL;
+symbol_get(symbol_t **value, char *key) {
+	symbol_t *result = NULL;
 #ifdef DUMP_SYMTAB
-    if ( result != NULL )
-        fprintf ( stderr, "Retrieving (%s,%d)\n", key, result->stack_offset );
+	if (result != NULL)
+		fprintf(stderr, "Retrieving (%s,%d)\n", key, result->stack_offset);
 #endif
 }
--- oving4/p2/src/vslc.c
+++ oving4/p2/src/vslc.c
@@ -4,79 +4,72 @@ static char *outfile = NULL;
 
 
 static void
-options ( int argc, char **argv )
-{
-    int32_t opt = 0;
-    while ( opt != -1 )
-    {
-        opt = getopt ( argc, argv, "f:o:" );
-        switch ( opt )
-        {
-            case -1:    /* No more options */
-                break;
+options(int argc, char **argv) {
+	int32_t opt = 0;
+	while (opt != -1) {
+		opt = getopt(argc, argv, "f:o:");
+		switch (opt) {
+		case -1:    /* No more options */
+			break;
 
-            case 'f':   /* Redirect input stream from file */
-                if ( freopen ( optarg, "r", stdin ) == NULL )
-                {
-                    fprintf (
-                        stderr, "Could not open input file '%s'\n", optarg
-                    );
-                    exit ( EXIT_FAILURE );
-                }
-                break;
+		case 'f':   /* Redirect input stream from file */
+			if (freopen(optarg, "r", stdin) == NULL) {
+				fprintf(
+				    stderr, "Could not open input file '%s'\n", optarg
+				);
+				exit(EXIT_FAILURE);
+			}
+			break;
 
-            case 'o':   /* Save filename, redirect stdout when src is ok */
-                outfile = STRDUP ( optarg );
-                break;
+		case 'o':   /* Save filename, redirect stdout when src is ok */
+			outfile = STRDUP(optarg);
+			break;
 
-            default:    /* Got some option we don't recognize */
-                fprintf ( stderr,
-                    "Usage: %s [-p] [-v #] [-f infile] [-o] outfile\n", argv[0]
-                );
-                exit ( EXIT_FAILURE );
-        }
+		default:    /* Got some option we don't recognize */
+			fprintf(stderr,
+			        "Usage: %s [-p] [-v #] [-f infile] [-o] outfile\n", argv[0]
+			       );
+			exit(EXIT_FAILURE);
+		}
 
-    }
+	}
 }
 
 
 int
-main ( int argc, char **argv )
-{
-    options ( argc, argv );
+main(int argc, char **argv) {
+	options(argc, argv);
 
-    symtab_init ();
-    yyparse();
-
-#ifdef DUMP_TREES
-    if ( (DUMP_TREES & 1) != 0 )
-        node_print ( stderr, root, 0 );
-#endif
-
-    simplify_tree ( &root, root );
+	symtab_init();
+	yyparse();
 
 #ifdef DUMP_TREES
-    if ( (DUMP_TREES & 2) != 0 )
-        node_print ( stderr, root, 0 );
+	if ((DUMP_TREES & 1) != 0)
+		node_print(stderr, root, 0);
 #endif
 
-    bind_names ( root );
+	simplify_tree(&root, root);
 
-    /* Parsing and semantics are ok, redirect stdout to file (if requested) */
-    if ( outfile != NULL )
-    {
-        if ( freopen ( outfile, "w", stdout ) == NULL )
-        {
-            fprintf ( stderr, "Could not open output file '%s'\n", outfile );
-            exit ( EXIT_FAILURE );
-        }
-        free ( outfile );
-    }
+#ifdef DUMP_TREES
+	if ((DUMP_TREES & 2) != 0)
+		node_print(stderr, root, 0);
+#endif
 
-    strings_output ( stdout );
+	bind_names(root);
 
-    destroy_subtree ( root );
-    symtab_finalize();
+	/* Parsing and semantics are ok, redirect stdout to file (if requested) */
+	if (outfile != NULL) {
+		if (freopen(outfile, "w", stdout) == NULL) {
+			fprintf(stderr, "Could not open output file '%s'\n", outfile);
+			exit(EXIT_FAILURE);
+		}
+		free(outfile);
+	}
 
-    exit ( EXIT_SUCCESS );
+	strings_output(stdout);
+
+	destroy_subtree(root);
+	symtab_finalize();
+
+	exit(EXIT_SUCCESS);
 }
