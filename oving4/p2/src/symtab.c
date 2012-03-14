2ecde80bfd1eaa406166558ba8ee5e1394f59e51
Einar Johan Trøan Sømåen <einarjohants@gmail.com>
2012-03-14 05:06:33 +0100

    OV4P2: Add some code

--- oving4/p2/src/symtab.c
+++ oving4/p2/src/symtab.c
@@ -8,48 +8,60 @@ static int32_t scopes_size = 16, scopes_index = -1;
 static int32_t values_size = 16, values_index = -1;
 static int32_t strings_size = 16, strings_index = -1;
 
-
-void
-symtab_init(void) {
+/* Called once before tree-traversal, sets up
+dynamic storage for a table of scopes, list of symbol_t-structures
+, and a table of strings. Pointers, sizes and limits of these arrays are
+already declared, but their size will be dynamically managed.*/
+void symtab_init(void) {
+	scopes = (hash_t**)malloc(sizeof(hash_t*) * scopes_size);
+	values = (symbol_t**)malloc(sizeof(symbol_t*) * values_size);
+	strings = (char**)malloc(sizeof(char*) * strings_size);
 }
 
-
-void
-symtab_finalize(void) {
+/* Called at end of execution, to remove tables and contents */
+void symtab_finalize(void) {
+	for (int i = 0; i <= scopes_index) {
+		free(scopes[i]);
+	}
+	free(scopes);
+	for (int i = 0; i <= values_index; i++) {
+		free(values[i])
+	}
+	free(values);
+	for (int i = 0; i <= strings_index; i++) {
+		free(strings[i]);
+	}
+	free(strings);
 }
 
-
-int32_t
-strings_add(char *str) {
+/* Appends a string to the table, resizing if appropriate,
+returns the index of the added string.*/
+int32_t strings_add(char *str) {
+	printf("Should add %s\n", str);
 	return 0;
 }
 
-
-void
-strings_output(FILE *stream) {
+/* Dumps the contents of the table to a provided output-stream */
+void strings_output(FILE *stream) {
 }
 
 
-void
-scope_add(void) {
+void scope_add(void) {
 }
 
 
-void
-scope_remove(void) {
+void scope_remove(void) {
 }
 
 
-void
-symbol_insert(char *key, symbol_t *value) {
+void symbol_insert(char *key, symbol_t *value) {
 #ifdef DUMP_SYMTAB
 	fprintf(stderr, "Inserting (%s,%d)\n", key, value->stack_offset);
 #endif
 }
 
 
-void
-symbol_get(symbol_t **value, char *key) {
+void symbol_get(symbol_t **value, char *key) {
 	symbol_t *result = NULL;
 #ifdef DUMP_SYMTAB
 	if (result != NULL)
